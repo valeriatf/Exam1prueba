5f35c03ea88b8cf53d194c234bdeb9def3e63dde
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-04-06 05:58:38 -0400

    Soluccion creada

---
+++ Web_prueba/Web_prueba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_prueba.MvcApplication" Language="C#" %>
---
+++ Web_prueba/Web_prueba/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_prueba.Startup))]
+namespace Web_prueba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
